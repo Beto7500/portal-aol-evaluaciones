db85935ac2831eeb73c617e42e7025e0b78214c1
Beto7500 <bto7500@gmail.com>
2026-09-21 22:09:23 -0600

    Renueva caché del portal

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='aol-evaluaciones-v2';
-const ASSETS=['./','index.html','styles.css','app.js','data.json','manifest.webmanifest','icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE='aol-evaluaciones-v5';
+const ASSETS=['./','index.html','styles.css?v=20260921-4','app.js?v=20260921-4','data.json','manifest.webmanifest','icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))))
